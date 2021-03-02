c0e949659cda0d1bcd5cf1e609929858fd385b43
vagrant <munawaer.ainiwaer@gmail.com>
2021-03-02 01:00:20 +0000

    Added all reviews to a single dictionary
    Co-Authored-By: Sam Gerrity <samantha.gerrity@gmail.com>

--- ratings.py
+++ ratings.py
@@ -1,4 +1,35 @@
 """Restaurant rating lister."""
 
+# open text file and assign to a variable
+# make an empty dictionary
+# add each line to the dictionary as key: value
+#   sort in alphabetical order
+# prints the entire dictionary 
+
+
 
 # put your code here
+
+def restaurant_ratings(text_file):
+
+    rating_file = open(text_file)
+    
+    reviews = {}
+
+    for line in rating_file:
+        
+        line = line.rstrip()
+        words = line.split(":")
+        
+        for word in words:
+
+            reviews[words[0]] = words[1]
+
+
+
+
+    print(reviews)
+
+
+
+
